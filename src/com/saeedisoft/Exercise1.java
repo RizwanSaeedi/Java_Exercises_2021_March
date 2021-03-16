4a9d2aaeccbf4c58b137487376b9caf7e801a029
Rizwan Saeedi <mrizwansaeedi3@gmail.com>
2021-03-16 07:55:08 +0500

    Initial commit

---
+++ src/com/saeedisoft/Exercise1.java
@@ -0,0 +1,15 @@
+// Write To Program To Print Counting From 1 to 10
+// and 10 to 1 using while loop , do while and for loop
+
+package com.saeedisoft;
+
+public class Exercise1
+{
+    public static void main(String args[])
+    {
+        for (int counter = 10; counter >= 1; counter = counter - 1)
+        {
+            System.out.println(counter);
+        }
+    }
+}
